cc5d61b6962866895f30245919967c45fe9da943
jAsOn <labanmacleod@gmail.com>
2024-01-08 11:59:45 +0900

    swiftui 성공

--- iosApp/iosApp/Home.swift
+++ iosApp/iosApp/Home.swift
@@ -14,36 +14,20 @@ import KMMViewModelSwiftUI
 struct HomeScreen: View {
     
     @StateViewModel var viewModel = HomeViewModel()
-    @State var count = 0
     
     var postTitle: String {
-        viewModel.state.first?.title ?? "No Title"
+        viewModel.allPosts.first?.title ?? "No Title"
     }
 
     var body: some View {
         VStack {
             Text("제목: " + postTitle)
-            Text("포스트 수: \(viewModel.state.count)")
-            Text("포스트 수: \(count)")
-            Button("Fetch Posts") {
-                viewModel.fetchAllPosts()
-                count = viewModel.getStateValue().count
-                print("dmdkdkdkdkdkdk")
-                print(count)
-            }
+            Text("포스트 수: \(viewModel.allPosts.count)")
+    
         }
         .task {
             viewModel.fetchAllPosts()
-            count = viewModel.getStateValue().count
-            print("dmdkdkdkdkdkdk")
-            print(count)
-           
         }
-        .onAppear {
-            viewModel.fetchAllPosts()
-            count = viewModel.getStateValue().count
-            print("dmdkdkdkdkdkdk")
-            print(count)
-        }
+    
     }
 }
--- shared/src/commonMain/kotlin/com/kake/base/viewmodel/HomeViewModel.kt
+++ shared/src/commonMain/kotlin/com/kake/base/viewmodel/HomeViewModel.kt
@@ -7,30 +7,24 @@ import com.kake.base.data.MongoSyncRepositoryImpl
 import com.kake.base.models.Post
 import com.kake.base.util.initMongoApp
 import com.rickclephas.kmm.viewmodel.KMMViewModel
+import com.rickclephas.kmm.viewmodel.MutableStateFlow
 import com.rickclephas.kmm.viewmodel.coroutineScope
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutinesState
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class HomeViewModel : KMMViewModel() {
-    private val _state = MutableStateFlow<List<Post>>(emptyList())
-    override fun onCleared() {
-        super.onCleared()
-    }
 
+    private val _allPosts = MutableStateFlow<List<Post>>(viewModelScope, emptyList())
     @NativeCoroutinesState
-    val state = _state.asStateFlow()
+    val allPosts = _allPosts.asStateFlow()
 
-    private val _allPosts: MutableState<List<Post>> =
-        mutableStateOf(emptyList())
-    val allPosts: State<List<Post>> = _allPosts
-    private val _searchedPosts: MutableState<List<Post>> =
-        mutableStateOf(emptyList())
-    val searchedPosts: State<List<Post>> = _searchedPosts
+    private val _searchedPosts = MutableStateFlow<List<Post>>(viewModelScope, emptyList())
+    @NativeCoroutinesState
+    val searchedPosts = _searchedPosts.asStateFlow()
     private val mongoSyncRepository = MongoSyncRepositoryImpl()
 
     init {
@@ -46,8 +40,6 @@ class HomeViewModel : KMMViewModel() {
                 println("fetchAllPosts() try")
                 mongoSyncRepository.readAllPosts().collect { // collectLatest 대신 collect 사용
                     _allPosts.value = it
-                    _state.value = it
-                    println("_state.value: ${_state.value.size}")
                 }
             } catch (e: Exception) {
                 println("fetchAllPosts() error: $e")
@@ -58,18 +50,13 @@ class HomeViewModel : KMMViewModel() {
 
     fun searchPostsByTitle(query: String) {
         viewModelScope.coroutineScope.launch {
-            withContext(Dispatchers.Main) {
-//                _searchedPosts.value = RequestState.Loading
-            }
             mongoSyncRepository.searchPostsByTitle(query = query).collectLatest {
                 _searchedPosts.value = it
             }
         }
     }
 
-    fun getStateValue() = state.value
-
     fun resetSearchedPosts() {
-//        _searchedPosts.value = RequestState.Idle
+        _searchedPosts.value = emptyList()
     }
 }
