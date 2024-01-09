efaac8689acc87c59a9c34d197c684d13fbd53bd
jAsOn <labanmacleod@gmail.com>
2024-01-09 12:27:50 +0900

    업데이트

--- iosApp/iosApp/Home.swift
+++ iosApp/iosApp/Home.swift
@@ -13,69 +13,6 @@ import KMMViewModelSwiftUI
 import WebKit
 
 
-struct HomeScreenView: View {
-    @StateViewModel var viewModel =  HomeViewModel()
-    @State private var searchQuery: String = ""
-    var webview: some View {
-//        WebView(url: URL(string: "https://www.naver.com")!)
-//        WebView(url: URL(string: "http://172.30.1.24:8080/posts/post?postId=658bb0f9e2b2aa68691fa9c4&showSections=false")!)
-        WebView(url: URL(string: "http://172.30.1.24:8080/posts/post?postId=659c9ef41c6e5612ae25e56f&showSections=false")!)
-            .navigationBarTitle("Details", displayMode: .inline)
-            .edgesIgnoringSafeArea(.bottom)
-    }
-    var body: some View {
-        webview
-        NavigationView {
-            List(viewModel.allPosts, id: \._id) { post in
-                PostCardView(post: post)
-            }
-//            .navigationTitle("Blog")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        // 메뉴 액션 구현
-                    }) {
-                        Image(systemName: "line.horizontal.3")
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        // 검색 액션 구현
-                    }) {
-                        Image(systemName: "magnifyingglass")
-                    }
-                }
-            }
-//            .searchable(text: $searchQuery)
-//            .onChange(of: searchQuery) { newValue in
-//                // The viewModel.searchPostsByTitle needs to be called on the main thread.
-//                DispatchQueue.main.async {
-//                    viewModel.searchPostsByTitle(query: newValue)
-//                }
-//            }
-        }
-    }
-}
-
-struct SearchView: View {
-    @Binding var searchQuery: String
-    var viewModel: HomeViewModel
-
-    var body: some View {
-        VStack {
-            TextField("검색어를 입력하세요", text: $searchQuery)
-                .padding()
-            
-            if viewModel.searchedPosts.isEmpty {
-                Text("검색 결과가 없습니다.")
-            } else {
-                PostCardsView(posts: viewModel.searchedPosts)
-            }
-        }
-        .padding()
-    }
-}
-
 // WebView를 SwiftUI에서 사용할 수 있도록 하는 뷰
 struct WebView: UIViewRepresentable {
     let url: URL
@@ -151,5 +88,39 @@ struct PostCardView: View {
             }
         }
         .padding(.vertical)
+    }
+}
+
+struct HomeScreenView: View {
+    @StateViewModel var viewModel =  HomeViewModel()
+    @State private var searchQuery: String = ""
+
+    var body: some View {
+        NavigationView {
+            PostCardsView(posts: viewModel.allPosts)
+                .navigationTitle("Blog")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button(action: {
+                            // 메뉴 액션 구현
+                        }) {
+                            Image(systemName: "line.horizontal.3")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            // 검색 액션 구현
+                        }) {
+                            Image(systemName: "magnifyingglass")
+                        }
+                    }
+                }
+                .searchable(text: $searchQuery)
+                .onChange(of: searchQuery) { newValue in
+                    DispatchQueue.main.async {
+                        viewModel.searchPostsByTitle(query: newValue)
+                    }
+                }
+        }
     }
 }
--- site/src/jvmMain/kotlin/com/kake/base/data/MongoDB.kt
+++ site/src/jvmMain/kotlin/com/kake/base/data/MongoDB.kt
@@ -31,7 +31,7 @@ fun initMongoDB(context:InitApiContext) {
 class MongoDB (private val context:InitApiContext): MongoRepository {
 
 //    private val client = MongoClient.create()
-    private val client = MongoClient.create(System.getenv("MONGODB_URI"))
+    private val client = MongoClient.create("mongodb+srv://labanmacleod:123456789sd@mycluster.4pvxxz0.mongodb.net/")
     private val database = client.getDatabase(DATABASE_NAME)
     private val userCollection = database.getCollection<User>("user")
     private val postCollection = database.getCollection<Post>("post")
